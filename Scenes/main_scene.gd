b1d8419a000ce81b99491404846ad8b6c987846a
Shuntoon <83526171+Shuntoon@users.noreply.github.com>
2023-03-20 01:57:47 -0400

    More Additions to Prototype
    -Added ability to pick colors on weapon parts
    -Added slight Visual Interest Via Lerps
    Added Ability to rotate model

--- Scenes/main_scene.gd
+++ Scenes/main_scene.gd
@@ -5,12 +5,26 @@ extends Node3D
 @onready var hilt_mesh = $SwordContainter/Hilt/HiltSpawn/HiltMesh
 @onready var handle_mesh = $SwordContainter/Handle/HandleSpawn/HandleMesh
 @onready var pummel_mesh = $SwordContainter/Pummel/PummelSpawn/PummelMesh
-@onready var color_pick = $ColorPick
+
+@onready var color_pick_blade = $ColorPickBlade
+@onready var color_pick_hilt = $ColorPickHilt
+@onready var color_pick_handle = $ColorPickHandle
+@onready var color_pick_pummel = $ColorPickPummel
+
+
+@export var transitionSpeed = .2
+@export var startSize = Vector3(.1,.1,.1)
+@export var colorPickPos = Vector2(200,0)
 
 var bladeIndex = 0
 var hiltIndex = 0
 var handleIndex = 0
 var pummelIndex = 0
+
+var bladeColor = Color.DARK_GRAY
+var hiltColor = Color.GOLD
+var handleColor = Color.DARK_RED
+var pummelColor = Color.GOLD
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,7 +34,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if bladeIndex > 2:
+	if bladeIndex > 3:
 		bladeIndex = 0
 	elif  bladeIndex < 0:
 		bladeIndex = 2
@@ -41,63 +55,153 @@ func _process(delta):
 		pummelIndex = 2
 		
 	blade_mesh.mesh = weaponParts.bladeMeshes[bladeIndex]
+	var bladeMeshMat = blade_mesh.get_active_material(0)
+	bladeColor = color_pick_blade.get_node("Panel/ColorPickerButton").color
+	bladeMeshMat.albedo_color = bladeColor
+	
 	hilt_mesh.mesh = weaponParts.HiltMeshes[hiltIndex]
+	var hiltMeshMat = hilt_mesh.get_active_material(0)
+	hiltColor = color_pick_hilt.get_node("Panel/ColorPickerButton").color
+	hiltMeshMat.albedo_color = hiltColor
+	
 	handle_mesh.mesh = weaponParts.HandleMeshes[handleIndex]
+	var handleMeshMat = handle_mesh.get_active_material(0)
+	handleColor = color_pick_handle.get_node("Panel/ColorPickerButton").color
+	handleMeshMat.albedo_color = handleColor
+	
 	pummel_mesh.mesh = weaponParts.PummelMeshes[pummelIndex]
+	var pummelMeshMat = pummel_mesh.get_active_material(0)
+	pummelColor = color_pick_pummel.get_node("Panel/ColorPickerButton").color
+	pummelMeshMat.albedo_color = pummelColor
 
 
 func _on_blade_next_button_pressed():
+	blade_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(blade_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	bladeIndex += 1
+	tween.tween_property(blade_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_blade_prev_button_pressed():
+	blade_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(blade_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	bladeIndex -= 1
+	tween.tween_property(blade_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_hilt_next_button_pressed():
+	hilt_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(hilt_mesh,"scale",Vector3(.67,.67,.67), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	hiltIndex += 1
+	tween.tween_property(hilt_mesh,"scale",Vector3(.6,.6,.6), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_hilt_prev_button_pressed():
+	hilt_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(hilt_mesh,"scale",Vector3(.67,.67,.67), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	hiltIndex -= 1
+	tween.tween_property(hilt_mesh,"scale",Vector3(.6,.6,.6), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_handle_next_button_pressed():
-	handleIndex += 1
+	handle_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(handle_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	handleIndex -= 1
+	tween.tween_property(handle_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_handle_prev_button_pressed():
-	handleIndex -=1
+	handle_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(handle_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	handleIndex -= 1
+	tween.tween_property(handle_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_pummel_next_button_pressed():
+	pummel_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(pummel_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pummelIndex += 1
+	tween.tween_property(pummel_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	pass # Replace with function body.
 
 
 func _on_pummel_prev_button_pressed():
-	handleIndex -= 1
-	pass # Replace with function body.
+	pummel_mesh.scale = Vector3(startSize)
+	var tween = create_tween()
+	tween.tween_property(pummel_mesh,"scale",Vector3(.57,.57,.57), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	pummelIndex -= 1
+	tween.tween_property(pummel_mesh,"scale",Vector3(.5,.5,.5), transitionSpeed).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 
 
 
 func _on_panel_gui_input(event):
 	if event is InputEvent:
 		if Input.is_action_just_pressed("Right_Click"):
-			print("Hi")
-			color_pick.visible = true
-			color_pick.position = get_viewport().get_mouse_position()
+			print("Blade")
+			color_pick_blade.visible = true
+			color_pick_handle.visible = false
+			color_pick_hilt.visible = false
+			color_pick_pummel.visible = false
+			color_pick_blade.position = get_viewport().get_mouse_position() + colorPickPos
 	pass # Replace with function body.
 
+func _on_hilt_area_gui_input(event):
+	if event is InputEvent:
+		if Input.is_action_just_pressed("Right_Click"):
+			print("Hilt")
+			color_pick_hilt.visible = true
+			color_pick_handle.visible = false
+			color_pick_blade.visible = false
+			color_pick_pummel.visible = false
+			color_pick_hilt.position = get_viewport().get_mouse_position() + colorPickPos
+	pass # Replace with function body.
+
+func _on_handle_area_gui_input(event):
+	if event is InputEvent:
+		if Input.is_action_just_pressed("Right_Click"):
+			print("Handle")
+			color_pick_handle.visible = true
+			color_pick_hilt.visible = false
+			color_pick_blade.visible = false
+			color_pick_pummel.visible = false
+			color_pick_handle.position = get_viewport().get_mouse_position() + colorPickPos
+	pass # Replace with function body.
+	
+func _on_pummel_area_gui_input(event):
+	if event is InputEvent:
+		if Input.is_action_just_pressed("Right_Click"):
+			print("Pummel")
+			color_pick_pummel.visible = true
+			color_pick_handle.visible = false
+			color_pick_blade.visible = false
+			color_pick_hilt.visible = false
+			color_pick_pummel.position = get_viewport().get_mouse_position() + colorPickPos
+	pass # Replace with function body.
 
 func _on_hud_gui_input(event):
 	if event is InputEvent:
 		if Input.is_action_just_pressed("Left_Click") or Input.is_action_just_pressed("Right_Click"):
-			color_pick.visible = false
+			color_pick_blade.visible = false
+			color_pick_hilt.visible = false
+			color_pick_pummel.visible = false
+			color_pick_handle.visible = false
 	pass # Replace with function body.
+
+
+
+
+
+
